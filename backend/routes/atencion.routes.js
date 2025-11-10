665299cbc30f2215a10eae4ec62dffc3d47ecade
MrDyslexia <mhhernand@gmail.com>
2025-11-09 23:35:24 -0300

    gitignore

--- backend/routes/atencion.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controlador = require('../controllers/atencion.controller.js');
-
-router.post('/', controlador.crearAtencion);
-
-module.exports = router;
